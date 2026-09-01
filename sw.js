b351069336f0b513d7e16456467827a494642edb
DAL0056 <dfs737800@gmail.com>
2026-09-01 07:18:02 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'flightboard-v4';
+const CACHE = 'flightboard-v17';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
